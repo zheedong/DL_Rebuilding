a628e634053137e9fd46c8f7bd20d6093070ed66
zheedong <zheedong@gmail.com>
2022-12-26 15:49:57 +0900

    DDPM complete

--- models/ddpm.py
+++ models/ddpm.py
@@ -37,3 +37,24 @@ class DenoiseDiffusion:
             esp = torch.randn_like(x0)
         mean, var = self.q_xt_x0(x0, t)
         return mean + (var ** 0.5) * eps
+
+    def p_sample(self, xt: torch.Tensor,
+                 t: torch.Tenson):
+        eps_theta = self.eps_model(xt, t)
+        alpha_bar = gather(self.alpha_bar, t)
+        alpha = gather(self.alpha, t)
+        eps_coef = (1 - alpha) / (1 - alpha_bar) ** .5
+        mean = 1 / (alpha ** 0.5) * (xt - eps_coef * eps_theta)
+        var = gather(self.sigma2, t)
+        eps = torch.randn(xt.shape, device=xt.device)
+        return mean + (var ** .5) * eps
+
+    def loss(self, x0: torch.Tensor, noise: Optional[torch.Tensor] = None):
+        batch_size = x0.shape[0]
+        t = torch.randint(0, self.n_steps, (batch_size,),
+                          device=x0.device, dtype=torch.long)
+        if noise is None:
+            noise = torch.randn_like(x0)
+        xt = self.q_sample(x0, t, eps=noise)
+        eps_theta = self.eps_model(xt, t)
+        return F.mse_loss(noise, eps_theta)
